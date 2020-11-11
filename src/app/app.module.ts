2043cf94b60ccdac8f9cea97df0769641eb9013b
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2020-11-10 20:44:08 -0500

    Adicion de router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
