58dc51a08fddcc538f7079b74a655487faf79ce8
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2020-12-13 00:21:12 -0500

    Mejoras menu, cambio de colores y cambio de imagen fondo

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,22 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scrolling, true)
+  }
+
+  scrolling=(s)=>{
+    let pxScroll = s.target.scrollingElement.scrollTop;
+    let alturaPantalla = window.innerHeight
+    let alturaEsperada = alturaPantalla * 0.8;
+    let estilo = "";
+    if (pxScroll == 0) {
+      estilo = "background-color: transparent !important;";
+    } else if (pxScroll > 0 && pxScroll <= alturaEsperada){
+      estilo = "background-color: rgba(12, 36, 97," + pxScroll / alturaEsperada +") !important;";
+    } else {
+      estilo = "background-color: rgba(12, 36, 97,1.0) !important;";
+    }
+    document.getElementById('navbarElement').setAttribute("style", estilo);
   }
 
 }
