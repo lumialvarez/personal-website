865606de3d4cc63d0709ccb9defbd50682cba137
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2020-12-29 21:09:38 -0500

    Modificacion de items menu y resumen

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,15 +13,15 @@ export class MainComponent implements OnInit {
     window.addEventListener('scroll', this.scrolling, true)
   }
 
-  scrolling=(s)=>{
+  scrolling = (s) => {
     let pxScroll = s.target.scrollingElement.scrollTop;
     let alturaPantalla = window.innerHeight
     let alturaEsperada = alturaPantalla * 0.7;
     let estilo = "";
     if (pxScroll == 0) {
       estilo = "background-color: transparent !important;";
-    } else if (pxScroll > 0 && pxScroll <= alturaEsperada){
-      estilo = "background-color: rgba(12, 36, 97," + pxScroll / alturaEsperada +") !important;";
+    } else if (pxScroll > 0 && pxScroll <= alturaEsperada) {
+      estilo = "background-color: rgba(12, 36, 97," + pxScroll / alturaEsperada + ") !important;";
     } else {
       estilo = "background-color: rgba(12, 36, 97,1.0) !important;";
     }
