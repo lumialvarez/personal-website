3bb3d5a8b60a6a69cc5af21f16e8a9eac84e2eab
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2021-04-18 21:03:33 -0500

    Pagina login ver inicial

---
+++ src/app/common/global-constants.ts
@@ -0,0 +1,5 @@
+export class GlobalConstants {
+    public static apiBasePath: string = "http://lmalvarez.com:9191/";
+    public static loginPath: string = "api/ext/v1/auth/login";
+    public static perfilPath: string = "api/ext/v1/auth/login";
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { GlobalConstants } from './../common/global-constants';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user = {
+    nombreUsuario: null,
+    password: null
+  };
+  public nombreUsuario: string = null;
+  public password: string = null;
 
-  constructor() { }
+  private apiLogin: string = null;
+  constructor(private httpClient: HttpClient) {
+    this.apiLogin = GlobalConstants.apiBasePath + GlobalConstants.loginPath;
+    console.log(this.apiLogin);
+  }
 
   ngOnInit(): void {
+  }
+
+  inicioSesion() {
+    console.log(this.user.nombreUsuario);
+    console.log(this.user.password);
+
+    this.httpClient.post(this.apiLogin, this.user).subscribe(
+      data => {
+        console.log(data);
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
