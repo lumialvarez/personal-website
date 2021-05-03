af06ec6f6ec09614a9d55e03898941afc59174b5
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2021-05-02 21:59:56 -0500

    Ajuste de url back

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-    public static apiBasePath: string = "http://lmalvarez.com:9191/";
+    public static apiBasePath: string = "https://services.lmalvarez.com//";
     public static loginPath: string = "api/ext/v1/auth/login";
     public static perfilPath: string = "api/ext/v1/auth/login";
 }
