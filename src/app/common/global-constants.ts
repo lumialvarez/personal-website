4f181f4d1e534b5cccde9167a087655aff9ddbe4
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2024-01-24 21:58:18 -0500

    Change to internal url

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-  private static apiBasePath = 'https://services.lmalvarez.com';
+  private static apiBasePath = 'go-api-gateway:9191';
   private static authorizationServicePath = '/authorization/api/v1';
   private static profileServicePath = '/profile/api/v1';
 
